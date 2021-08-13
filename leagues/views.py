1eedee7811191b0be2f887e538eee5e412b64d61
Patricio Fuentealba <patricio.fuentealba.feliu@gmail.com>
2021-08-12 21:53:27 -0400

    hasta el ejercicio 6 listo

--- leagues/views.py
+++ leagues/views.py
@@ -4,10 +4,25 @@ from .models import League, Team, Player
 from . import team_maker
 
 def index(request):
+	baseball_liga=League.objects.filter(sport='Baseball').all()
+	ligas_femeninas=League.objects.filter(name__contains='Women').all()
+	ligas_hockey=League.objects.filter(sport__contains='hockey').all()
+	ligas_no_futbol=League.objects.exclude(sport='Football').all()
+	ligas_conferencia=League.objects.filter(name__contains='conference').all()
+	ligas_atlantica=League.objects.filter(name__contains='atlantic').all()
+	ligas_dallas=[liga for liga in  League.objects.all() if liga in Team.objects.all().filter(location='Dallas')]
+
 	context = {
 		"leagues": League.objects.all(),
 		"teams": Team.objects.all(),
 		"players": Player.objects.all(),
+		"baseball_liga":baseball_liga,
+		"ligas_femeninas":ligas_femeninas,
+		"ligas_hockey":ligas_hockey,
+		"ligas_no_futbol":ligas_no_futbol,
+		"ligas_conferencia":ligas_conferencia,
+		"ligas_atlantica":ligas_atlantica,
+		"ligas_dallas":ligas_dallas
 	}
 	return render(request, "leagues/index.html", context)
 
