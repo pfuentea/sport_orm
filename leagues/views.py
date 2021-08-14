dce97bba403d0a7ee9a1abb3604638818277e592
Patricio Fuentealba <patricio.fuentealba.feliu@gmail.com>
2021-08-14 11:01:20 -0400

    16 consultas listas

--- leagues/views.py
+++ leagues/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import League, Team, Player
-
+from django.db.models import Q
 from . import team_maker
 
 def index(request):
@@ -10,7 +10,17 @@ def index(request):
 	ligas_no_futbol=League.objects.exclude(sport='Football').all()
 	ligas_conferencia=League.objects.filter(name__contains='conference').all()
 	ligas_atlantica=League.objects.filter(name__contains='atlantic').all()
-	ligas_dallas=[liga for liga in  League.objects.all() if liga in Team.objects.all().filter(location='Dallas')]
+	ligas_dallas=League.objects.filter(teams__location='Dallas')
+	teams_raptors=Team.objects.filter(team_name__contains='raptor').all()
+	teams_city=Team.objects.filter(location__contains='city').all()
+	teams_t=Team.objects.filter(location__startswith='t').all()
+	#10
+	teams_order_location=Team.objects.all().order_by('location')
+	teams_order_team_inv=Team.objects.all().order_by('-team_name')
+	jugadores_cooper=Player.objects.filter(last_name__contains='Cooper').all()
+	jugadores_joshua=Player.objects.filter(first_name__contains='Joshua').all()
+	jugadores_cooper_joshua=[pl for pl in Player.objects.filter(last_name__contains='Cooper').all() if pl not in jugadores_joshua]
+	jugadores_alex_wyatt=Player.objects.filter(Q(first_name__contains='Wyatt')|Q(first_name__contains='Alexander')).all()
 
 	context = {
 		"leagues": League.objects.all(),
@@ -22,7 +32,16 @@ def index(request):
 		"ligas_no_futbol":ligas_no_futbol,
 		"ligas_conferencia":ligas_conferencia,
 		"ligas_atlantica":ligas_atlantica,
-		"ligas_dallas":ligas_dallas
+		"ligas_dallas":ligas_dallas,
+		"teams_raptors":teams_raptors,
+		"teams_city":teams_city,
+		"teams_t":teams_t,
+		"teams_order_location":teams_order_location,
+		"teams_order_team_inv":teams_order_team_inv,
+		"jugadores_cooper":jugadores_cooper,
+		"jugadores_joshua":jugadores_joshua,
+		"jugadores_cooper_joshua":jugadores_cooper_joshua,
+		"jugadores_alex_wyatt":jugadores_alex_wyatt
 	}
 	return render(request, "leagues/index.html", context)
 
